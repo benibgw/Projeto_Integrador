9755d4bd086d547f1bb7e8b8c4d0a2c1d9af512f
benibgw <beniciogabrielwendt266@gmail.com>
2025-08-29 20:31:38 -0300

    New web interface

--- Webserver/Webserver.ino
+++ Webserver/Webserver.ino
@@ -1,8 +1,8 @@
 #include <WiFi.h>
 #include <WebServer.h>
 
-const char* ssid = "Seu_SSID";
-const char* password = "Seu_Password";
+const char* ssid = "BENICIO";
+const char* password = "9723raddatz";
 
 WebServer server(80);
 
@@ -20,9 +20,18 @@ void WifiConnection() {
   Serial.println("Connected.");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
+
+  // Rotas
   server.on("/", handleRoot);
+  server.on("/forward", handleForward);
+  server.on("/left", handleLeft);
+  server.on("/right", handleRight);
+  server.on("/start", handleStart);
+  server.on("/undo", handleUndo);
+  server.on("/clear", handleClear);
+
   server.begin();
-  Serial.print("Server started.");
+  Serial.println("Server started.");
 }
 
 bool CheckWifiConnection() {
@@ -35,14 +44,62 @@ bool CheckWifiConnection() {
   }
 }
 
+String getPage() {
+  return R"rawliteral(
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='UTF-8'>
+<title>Controle do Robô</title>
+</head>
+<body style='text-align:center; font-family: Arial;'>
+<h2>Controle do Robô</h2>
+
+<button onclick="location.href='/forward'">Frente</button><br><br>
+<button onclick="location.href='/left'">Esquerda</button>
+<button onclick="location.href='/right'">Direita</button><br><br>
+
+<button onclick="location.href='/start'">Start</button><br><br>
+<button onclick="location.href='/undo'">Desfazer</button>
+<button onclick="location.href='/clear'">Limpar</button>
+
+</body>
+</html>
+)rawliteral";
+}
+
 void handleRoot() {
-  server.send(200, "text/html",
-              "<!DOCTYPE html>"
-              "<html>"
-              "<head><title>IoT</title></head>"
-              "<body style='font-family: Arial; text-align: center; margin-top: 50px;'>"
-              "<h1>IoT - Projeto Integrador</h1>"
-              "</body></html>");
+  server.send(200, "text/html", getPage());
+}
+
+void handleForward() {
+  Serial.println("Comando: Frente");
+  server.send(200, "text/html", getPage());
+}
+
+void handleLeft() {
+  Serial.println("Comando: Esquerda");
+  server.send(200, "text/html", getPage());
+}
+
+void handleRight() {
+  Serial.println("Comando: Direita");
+  server.send(200, "text/html", getPage());
+}
+
+void handleStart() {
+  Serial.println("Comando: Start");
+  server.send(200, "text/html", getPage());
+}
+
+void handleUndo() {
+  Serial.println("Comando: Undo (remover último)");
+  server.send(200, "text/html", getPage());
+}
+
+void handleClear() {
+  Serial.println("Comando: Clear (limpar fila)");
+  server.send(200, "text/html", getPage());
 }
 
 void setup() {
